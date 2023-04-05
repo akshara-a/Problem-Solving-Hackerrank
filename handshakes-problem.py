43b72f241e06940f1e310c7a1c8c7cb45fe70758
Akshara A <68069921+akshara-a@users.noreply.github.com>
2023-04-05 21:22:01 +0530

    Create handshakes-problem.py

---
+++ handshakes-problem.py
@@ -0,0 +1,9 @@
+# Handshakes = n*(n - 1)/2
+# Because each of the n people can shake hands with n - 1 people (they would not shake their own hand)n
+# And the handshake between two people is not counted twice. This formula can be used for any number of people.
+
+n = int(input())
+
+handshakes = (n*(n-1))//2
+
+print(handshakes)
