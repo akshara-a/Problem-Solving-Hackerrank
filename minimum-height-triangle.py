42af160c46f1f79dce335d269b6b4a6064817e43
Akshara A <68069921+akshara-a@users.noreply.github.com>
2023-04-05 21:35:40 +0530

    Create minimum-height-triangle.py

---
+++ minimum-height-triangle.py
@@ -0,0 +1,9 @@
+# The task is to find the smallest integer height of the triangle 
+
+import math
+
+trianglebase,area = [int(x) for x in input().split()]
+
+triangleheight = (2*area)/trianglebase
+
+print(math.ceil(triangleheight))
